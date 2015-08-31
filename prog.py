721a84d367ecad8dea4d1faa2ebca01ae69e63a6
Stephen Tu <stephent@berkeley.edu>
2015-08-31 16:59:34 -0400

    check all ints

--- prog.py
+++ prog.py
@@ -3,10 +3,10 @@
 from xpr import create_execution_engine
 from xpr.ast import *
 
-from ctypes import CFUNCTYPE, c_double
+from ctypes import CFUNCTYPE, c_double, c_longlong
 
 
-def main():
+def test1():
     x = Variable("x", float)
     y = Variable("y", float)
     f = Function(
@@ -14,7 +14,6 @@ def main():
             params=(x, y),
             rettype=float,
             expr=10 * x + y)
-    print f.expr
     engine = create_execution_engine()
     module = f.compile(engine)
     func_ptr = engine.get_pointer_to_function(module.get_function("foo"))
@@ -22,5 +21,21 @@ def main():
     assert cfunc(1.0, 3.5) == 10 * 1.0 + 3.5
 
 
+def test2():
+    x = Variable("x", int)
+    y = Variable("y", int)
+    f = Function(
+            name="foo",
+            params=(x, y),
+            rettype=int,
+            expr=10 * x - y)
+    engine = create_execution_engine()
+    module = f.compile(engine)
+    func_ptr = engine.get_pointer_to_function(module.get_function("foo"))
+    cfunc = CFUNCTYPE(c_longlong, c_longlong, c_longlong)(func_ptr)
+    assert cfunc(5, 3) == 10 * 5 - 3
+
+
 if __name__ == '__main__':
-    main()
+    test1()
+    test2()
