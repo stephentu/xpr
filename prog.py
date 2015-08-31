5abe95570232f4388bf45be4381d3d6271a0d46e
Stephen Tu <stephent@berkeley.edu>
2015-08-31 18:21:30 -0400

    better testing

--- prog.py
+++ prog.py
@@ -5,6 +5,12 @@ from xpr.ast import *
 
 from ctypes import CFUNCTYPE, c_double, c_longlong
 
+import math
+
+
+def almost_eq(x, y, tol=1e-8):
+    return math.fabs(x - y) <= tol
+
 
 def test1():
     x = Variable("x", float)
@@ -18,7 +24,7 @@ def test1():
     module = f.compile(engine)
     func_ptr = engine.get_pointer_to_function(module.get_function("foo"))
     cfunc = CFUNCTYPE(c_double, c_double, c_double)(func_ptr)
-    assert cfunc(1.0, 3.5) == 10 * 1.0 + 3.5
+    assert almost_eq(cfunc(1.0, 3.5), 10 * 1.0 + 3.5)
 
 
 def test2():
@@ -47,12 +53,11 @@ def test3():
     module = f.compile(engine)
     func_ptr = engine.get_pointer_to_function(module.get_function("foo"))
     cfunc = CFUNCTYPE(c_double, c_double)(func_ptr)
-    print cfunc(3.5)
-    import math
-    print 10.0 * math.log(3.5) + 1.0
+
+    assert almost_eq(cfunc(3.5), 10.0 * math.log(3.5) + 1.0)
 
 
 if __name__ == '__main__':
-    #test1()
-    #test2()
+    test1()
+    test2()
     test3()
